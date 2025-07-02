0f74e54dbe598e9c90e38eb3df5114bcec6f4342
Mark Hallett <markhallett5@outlook.com>
2025-07-02 22:55:58 +0100

    intalled gunicorn and updated settings.py

--- projectPP3/projectPP3/settings.py
+++ projectPP3/projectPP3/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3b&0%5w5o_u)hr*z4&i=vm1nw%z(l3d9y#zk_@bv5+((rq8y5)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',]
 
 
 # Application definition
