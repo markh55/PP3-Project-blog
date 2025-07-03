c0686a52b2060ccded2b8fbb773ffb3079b7af58
Mark Hallett <markhallett5@outlook.com>
2025-07-03 23:56:39 +0100

    added status field and fixed typo within settings.py and related files.

--- projectPP3/settings.py
+++ projectPP3/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',
+    'projectPP3.blog',
 ]
 
 MIDDLEWARE = [
