4e2cd82e900faa8d3e1a8963e46dfa2794d0ca1c
Mark Hallett <markhallett5@outlook.com>
2025-07-06 20:36:55 +0100

    set up homepage to have login / logout buttons

--- projectPP3/settings.py
+++ projectPP3/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'projectPP3.blog',
 ]
 
@@ -53,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'projectPP3.urls'
--- projectPP3/urls.py
+++ projectPP3/urls.py
@@ -20,4 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('projectPP3.blog.urls', 'blog'), namespace='blog')),
+    path('accounts/', include('allauth.urls')),
+
 ]
