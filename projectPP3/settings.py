08a457352c50df2fe5d562ec827eed99ecc350ad
Mark Hallett <markhallett5@outlook.com>
2025-07-09 00:39:21 +0100

    Merge local and Heroku databases to use a single shared database

--- projectPP3/settings.py
+++ projectPP3/settings.py
@@ -110,10 +110,11 @@ WSGI_APPLICATION = 'projectPP3.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgres://mark:Arlo102018@localhost:5432/pp3db',
+        default=os.getenv('DATABASE_URL', 'postgres://mark:Arlo102018@localhost:5432/pp3db'),
         conn_max_age=600
     )
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
