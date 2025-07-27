4e6dae80a2a96278ac8b5e90bcdebcb3f9a96fcb
Mark Hallett <markhallett5@outlook.com>
2025-07-28 00:27:58 +0100

    updated forms.py to show label = Recipient's email

--- projectPP3/blog/forms.py
+++ projectPP3/blog/forms.py
@@ -9,11 +9,11 @@ class CommentForm(forms.ModelForm):
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
-    to = forms.EmailField()
+    to = forms.EmailField(label="Recipient's email")
     comments = forms.CharField(required=False, widget=forms.Textarea)
 
 class RecommendPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
-    to = forms.EmailField()
+    to = forms.EmailField(label="Recipient's email")
     comments = forms.CharField(required=False, widget=forms.Textarea)
